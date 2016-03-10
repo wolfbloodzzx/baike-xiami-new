8bc869642e2204145261d061833981c287a922d4
Zhang Zixing <zhangzi_xing@126.com>
2016-03-10 11:50:43 +0800

    爬虫结构

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import spider.Spider
+
+if __name__ == "__main__":
+
+    spider_thread = Spider(1)
+    spider.strat()
---
+++ spider.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding=utf-8
+import threading
+
+class Spider(threading.Thread):
+
+    def __init__(self,url):
+        pass
+
+    def run(self):
+        pass
+
+    def __stop(self):
+        pass
